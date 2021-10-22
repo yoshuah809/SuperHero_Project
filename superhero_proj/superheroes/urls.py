0d37dd645179ed5b969a7a920344529ffb949d4d
unknown <yoshuah809@gmail.com>
2021-10-22 15:48:16 -0500

    added the detail function

--- superhero_proj/superheroes/urls.py
+++ superhero_proj/superheroes/urls.py
@@ -1,8 +1,9 @@
-from . import views
 from django.urls import path
+from . import views
 
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name= 'index'),
-    path ('new/', views.create, name = 'create')
+    path('new/', views.create, name= 'create'),
+    path ('<int:hero_id>/', views.detail, name= 'detail')
 ]
--- superhero_proj/superheroes/views.py
+++ superhero_proj/superheroes/views.py
@@ -6,32 +6,37 @@ from . forms import CreateHeroForm
 
 
 # Create your views here.
+# Create your views here.
 def index(request):
     all_heroes = Superhero.objects.all()
-    contex = {
+    context = {
         'all_heroes': all_heroes
         }
-    return render(request, 'superheroes/index.html', contex)
+    return render(request, 'superheroes/index.html', context)
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request,'superheroes/detail.html', context)
 
 def create(request):
     if request.method == 'POST':
-        #Save content in new database
-        # Return to index page
-        form = CreateHeroForm(request)
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catch_phrase')
+        new_hero = Superhero(name=name, 
+                                alter_ego=alter_ego, 
+                                primary_ability=primary, 
+                                secondary_ability=secondary, 
+                                catch_phrase=catchphrase)
+        new_hero.save()
 
-        # name = request.POST.get('name')
-        # alter_ego = request.POST.get('alter_ego')
-        # primary = request.POST.get('primary')
-        # secondary = request.POST.get('secondary')
-        # catchphrase = request.POST.get('catch_phrase')
-        # new_hero = Superhero(name=name, 
-        #                         alter_ego=alter_ego, 
-        #                         primary_ability=primary, 
-        #                         secondary_ability=secondary, 
-        #                         catch_phrase=catchphrase)
-        #new_hero.save()
-        return render(request, 'superheroes:index', {'createheroform':form})
-        #return HttpResponseRedirect(reverse('superheroes:index'))
-
-
-        # return render(request, 'superheroes/create.html')  
+         # Return to index page
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')  
+  
